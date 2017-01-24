cb3b5384b3f5618a1417044c4b104900dc4ab82a
ANDRISPC\andras.kuhn <eyewitness4560@gmail.com>
2017-01-24 12:38:51 +0100

    Tank cpp and TankController

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "Public/Tank.h"
+#include "TankPlayerController.generated.h" //must be last included
+
+/**
+ *
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATank* GetControlledTank() const;
+
+};
